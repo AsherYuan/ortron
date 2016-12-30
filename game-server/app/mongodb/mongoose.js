8f9cde270dddc089a8512a8c3a38ce1b54cee758
AsherYuan <acegikm3384@sina.com>
2016-12-30 10:50:18 +0800

    'fix'

--- game-server/app/mongodb/mongoose.js
+++ game-server/app/mongodb/mongoose.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://122.225.88.66:27017/otron');
+//mongoose.connect('mongodb://122.225.88.66:27017/otron');
+mongoose.connect('mongodb://127.0.0.1:27017/otron');
 mongoose.Promise = require('bluebird');
 module.exports = mongoose;
